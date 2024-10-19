03ab44c42573d06cd586308c8b70e58502532c27
Renan Piva <renan6589@gmail.com>
2024-10-18 21:54:02 -0300

    Teste

--- Echos of Time/Assets/Scripts/Menu.cs
+++ Echos of Time/Assets/Scripts/Menu.cs
@@ -9,10 +9,14 @@ public class Menu : MonoBehaviour
     [SerializeField] private GameObject opcoes;
     [SerializeField] private GameObject menu;
 
+    private int oi = 0;
+    private int oi2 = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         FecharOpcoes();
+        
     }
 
     // Update is called once per frame
